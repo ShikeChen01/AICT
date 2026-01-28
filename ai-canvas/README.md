53290bb88ec945648f3fef9348c092e11cbbb2f3
Shike Chen <40473396+MikeLoveGame@users.noreply.github.com>
2026-01-27 22:54:12 -0500

    this is the proper VS Code extension setup

--- ai-canvas/README.md
+++ ai-canvas/README.md
@@ -1,6 +1,6 @@
-# AICVS README
+# ai-canvas README
 
-This is the README for your extension "AICVS". After writing up a brief description, we recommend including the following sections.
+This is the README for your extension "ai-canvas". After writing up a brief description, we recommend including the following sections.
 
 ## Features
 
--- ai-canvas/src/extension.ts
+++ ai-canvas/src/extension.ts
@@ -8,15 +8,15 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "AICVS" is now active!');
+	console.log('Congratulations, your extension "ai-canvas" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('AICVS.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('ai-canvas.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from AI Canvas!');
+		vscode.window.showInformationMessage('Hello World from AI-Canvas!');
 	});
 
 	context.subscriptions.push(disposable);
